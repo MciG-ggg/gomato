bc3dfc477d92eea63c00a33aaa1db9b68e216130
MciG-ggg <20949382132@qq.com>
2025-07-12 09:10:21 +0800

    feat(dependencies): 更新依赖库以支持桌面通知功能\n\n- 在go.mod中新增github.com/go-toast/toast和github.com/nu7hatch/gouuid依赖\n- 更新go.sum以反映新增依赖的版本信息

--- pkg/notice/notice.go
+++ pkg/notice/notice.go
@@ -16,12 +16,12 @@ func SendNotification(title, message string) {
 	case "linux":
 		exec.Command("notify-send", "-t", fmt.Sprintf("%d", noticeMaintainTime), title, message).Run()
 	case "windows":
-		toast.Notification{
+		(&toast.Notification{
 			AppID:    "Gomato",
 			Title:    title,
 			Message:  message,
 			Duration: toast.Short,
-		}.Push()
+		}).Push()
 
 	}
 }
